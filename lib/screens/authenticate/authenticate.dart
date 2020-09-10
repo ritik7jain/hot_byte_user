3e0c255f830961e076696c7fb21fcf2e3a02ae07
Kunal Mod <devkunalmod@gmail.com>
2020-09-11 02:15:01 +0530

    ssh1 added for kunal

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,15 +1,15 @@
-import 'package:flutter/material.dart';
-import 'package:hot_byte_user/login_page.dart';
-
-class Authenticate extends StatefulWidget {
-  @override
-  _AuthenticateState createState() => _AuthenticateState();
-}
-
-class _AuthenticateState extends State<Authenticate> {
-  @override
-  Widget build(BuildContext context) {
-    return LoginPage();
-      
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:hot_byte_user/login_page.dart';
+//
+// class Authenticate extends StatefulWidget {
+//   @override
+//   _AuthenticateState createState() => _AuthenticateState();
+// }
+//
+// class _AuthenticateState extends State<Authenticate> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return LoginPage();
+//
+//   }
+// }
--- lib/signin.dart
+++ lib/signin.dart
@@ -3,59 +3,53 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAuth _auth = FirebaseAuth.instance;
 final GoogleSignIn googleSignIn = GoogleSignIn();
-Future<String> inputData() async {
-    final FirebaseUser user = await FirebaseAuth.instance.currentUser();
-    final String uid = user.uid.toString();
-  return uid;
-  }
 
 String name;
 String email;
 String imageUrl;
 
-
 Future<String> signInWithGoogle() async {
-  try{final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
-  final GoogleSignInAuthentication googleSignInAuthentication =
-      await googleSignInAccount.authentication;
+  try {
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
 
-  final AuthCredential credential = GoogleAuthProvider.getCredential(
-    accessToken: googleSignInAuthentication.accessToken,
-    idToken: googleSignInAuthentication.idToken,
-  );
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
 
-  final AuthResult authResult = await _auth.signInWithCredential(credential);
-  final FirebaseUser user = authResult.user;
-  assert(user.email != null);
-  assert(user.displayName != null);
-  assert(user.photoUrl != null);
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+    assert(user.email != null);
+    assert(user.displayName != null);
+    assert(user.photoUrl != null);
 
-  name = user.displayName;
-  email = user.email;
-  imageUrl = user.photoUrl;
-  
+    name = user.displayName;
+    email = user.email;
+    imageUrl = user.photoUrl;
 
-  assert(!user.isAnonymous);
-  assert(await user.getIdToken() != null);
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
 
-  final FirebaseUser currentUser = await _auth.currentUser();
-  assert(user.uid == currentUser.uid);
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
 
-  return 'signInWithGoogle succeeded: $user';}
-  catch(e)
-  {
+    return 'signInWithGoogle succeeded: $user';
+  } catch (e) {
     print(e.toString());
     return null;
   }
 }
-Future signOutGoogle() async{
-  try{
-    await _auth.signOut().catchError((error){
-       print(error.toString());
-   });
-   await googleSignIn.signOut();
+
+Future signOutGoogle() async {
+  try {
+    await _auth.signOut().catchError((error) {
+      print(error.toString());
+    });
+    await googleSignIn.signOut();
+  } catch (e) {
+    print(e.toString());
+    return null;
+  }
 }
-catch(e){
-  print(e.toString());
-  return null;
-}}
