52397ec4bb72831c387bf0e6074872181b67a706
Ritik Jain <jritik324@gmail.com>
2020-09-10 23:27:58 +0530

    login page and ui added

---
+++ lib/constants/constants.dart
@@ -0,0 +1,55 @@
+const FOOD_DATA = [
+  {
+    "name":"Burger",
+    "brand":"Hawkers",
+    "price":"30 Rs" ,
+    "image":"burger.png"
+  },{
+    "name":"Cheese Dip",
+    "brand":"Hawkers",
+    "price":"70 Rs",
+    "image":"cheese_dip.png"
+  },
+  {
+    "name":"Cola",
+    "brand":"Mcdonald",
+    "price":"50 Rs",
+    "image":"cola.png"
+  },
+  {
+    "name":"Fries",
+    "brand":"Mcdonald",
+    "price":"40 Rs",
+    "image":"fries.png"
+  },
+  {
+    "name":"Ice Cream",
+    "brand":"Ben & Jerry's",
+    "price":"80 Rs",
+    "image":"ice_cream.png"
+  },
+  {
+    "name":"Noodles",
+    "brand":"Hawkers",
+    "price":"70 Rs",
+    "image":"noodles.png"
+  },
+  {
+    "name":"Pizza",
+    "brand":"Dominos",
+    "price":"110 Rs",
+    "image":"pizza.png"
+  },
+  {
+    "name":"Sandwich",
+    "brand":"Hawkers",
+    "price":"60 Rs",
+    "image":"sandwich.png"
+  },
+  {
+    "name":"Wrap",
+    "brand":"Subway",
+    "price":"30 Rs",
+    "image":"wrap.png"
+  }
+];
---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,19 @@
+// import 'package:flutter/material.dart';
+// import 'package:hot_byte_user/first_screen.dart';
+// import 'package:hot_byte_user/login_page.dart';
+// import 'package:hot_byte_user/screens/authenticate/authenticate.dart';
+// import 'package:provider/provider.dart';
+
+
+// class Wrapper extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     final user = Provider.of<User>(context);
+//     if (user==null){
+//       return Authenticate();}
+//     else
+//     {
+//       return MyHomePage();
+//     }
+//   }
+// }
